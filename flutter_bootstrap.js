dc50b2dcea44b5209d556f2599b90deeddb0a5ea
mostafa-hashem <mostafa-hashem@users.noreply.github.com>
2025-07-01 01:14:43 +0000

    deploy: cfe2ef4ec964acd7de6a5fe47929eb5d0ba3940e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3668615648"
+    serviceWorkerVersion: "3480263117"
   }
 });
